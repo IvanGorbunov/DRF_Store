363df652cc403a2e1fd9531a384220baa8a4516a
IvanGorbunov <ivan_0110@mail.ru>
2022-04-03 17:25:32 +0300

    added model Product

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from store.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    verbose_name_plural = 'Номенклатура'
+    list_display = (
+        'id',
+        'title',
+        'cost_price',
+        'price',
+        'amount',
+    )
+    list_display_links = (
+        'id',
+        'title',
+    )
--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from store.utils import DateModelMixin
+
+
+class Product(models.Model):
+    """
+    Модель: Номенклатура
+    """
+    title = models.CharField('Название', max_length=250)
+    cost_price = models.FloatField("Себестоимость", blank=True, default=0)
+    price = models.FloatField("Цена", blank=True, default=0)
+    amount = models.FloatField("Количество", blank=True, default=0)
+
+    class Meta:
+        verbose_name = 'Номенклатура'
+        verbose_name_plural = 'Номенклатура'
